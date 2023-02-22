cb38bfb7253990734fdda73da6c67d96593717dd
Shubh Patel <shubhpatel891@gmail.com>
2023-02-22 22:37:13 +0300

    Created More Pages

--- React/src/views/Surveys.jsx
+++ React/src/views/Surveys.jsx
@@ -1,6 +1,36 @@
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import React from "react";
+import TButton from "../components/core/TButton";
 import PageComponent from "../components/PageComponent";
+import SurveyListItem from "../components/SurveyListItem";
+import { useStateContext } from "../contexts/ContextProvider";
 
-export default function () {
-    return <PageComponent title="Survyes">Surveys Content</PageComponent>;
+export default function Surveys() {
+    const { surveys } = useStateContext();
+    console.log(surveys);
+
+    const onDeleteClick = () => {
+        console.log("On Delete Click");
+    };
+    return (
+        <PageComponent
+            title="Surveys"
+            buttons={
+                <TButton color="green" to="/surveys/create">
+                    <PlusCircleIcon className="h-6 w-6 mr-2" />
+                    Create New
+                </TButton>
+            }
+        >
+            <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+                {surveys.map((survey) => (
+                    <SurveyListItem
+                        survey={survey}
+                        key={survey.id}
+                        onDeleteClick={onDeleteClick}
+                    />
+                ))}
+            </div>
+        </PageComponent>
+    );
 }
